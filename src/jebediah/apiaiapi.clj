d070edf434d1c8dd11d565ea11778fd60e5ddb15
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-06-16 01:53:58 +0200

    Add demos

--- src/jebediah/apiaiapi.clj
+++ src/jebediah/apiaiapi.clj
@@ -1,14 +1,22 @@
 (ns jebediah.apiaiapi)
 
-(defmulti dispatch-action #(get-in % [:result :action]))
+(defmulti dispatch-action
+          "Executes an action"
+          #(get-in % [:result :action]))
 
-(defmacro defaction [name params & body]
+(defmacro defaction
+  "Creates and registers a new action. Name this action exactly like you did in api.ai!"
+  [name params & body]
   `(defmethod dispatch-action ~(str name) ~params
      ~@body))
 
-(defn simple-apiai-response [speech]
-   {:speech speech
-    :displayText speech
-    :data []
-    :contextOut []
-    :source ""})
+(defn simple-speech-response
+  "Returns a valid api.ai map with 'speech' and 'displayText' set to speech."
+  [& speech]
+  {:speech (apply str speech)
+   :displayText (apply str speech)
+   :data []
+   :contextOut []
+   :source ""})
+
+(defmethod dispatch-action :default [r] (simple-speech-response "Action: " (get-in r [:result :action]) " not implemented! Sorry."))
--- src/jebediah/hello.clj
+++ src/jebediah/hello.clj
@@ -8,6 +8,8 @@
 (defaction hello_world [_] (-> "https://dbas.cs.uni-duesseldorf.de/api/hello"
                                (slurp-json)
                                (:message)
-                               (simple-apiai-response)))
+                               (simple-speech-response)))
 
-(defaction echo_name [request-body] (simple-apiai-response (str "Hello " (get-in request-body [:result :parameters :given-name]))))
+(defaction echo_name [request-body] (simple-speech-response "Hello " (get-in request-body [:result :parameters :given-name])))
+
+(defaction how_long [request-body] (simple-speech-response "The word is " (count (get-in request-body [:result :parameters :word])) " characters long."))
