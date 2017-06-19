bf1fe646870acdcd19de22cb8937372285749a2e
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-06-19 10:20:28 +0200

    Update to clojure's alpha 1.9

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jebediah "0.1.0-SNAPSHOT"
+(defproject jebediah "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.logging "0.4.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
@@ -11,7 +11,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.6.1"]]
   :plugins [[lein-ring "0.9.7"]]
+
   :ring {:handler jebediah.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}})
