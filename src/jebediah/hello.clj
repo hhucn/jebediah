51afa641d6019a48b3dbb118599e2a345f9808d6
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-06-19 10:44:18 +0200

    Reformat code, ignore emacs temp file

--- src/jebediah/hello.clj
+++ src/jebediah/hello.clj
@@ -2,14 +2,17 @@
   (:require [jebediah.apiaiapi :refer :all]
             [clojure.data.json :as json]))
 
-(defmacro slurp-json [f & opts]
+(defn slurp-json [f & opts]
   `(json/read-str (slurp ~f ~@opts) :key-fn keyword))
 
-(defaction hello_world [_] (-> "https://dbas.cs.uni-duesseldorf.de/api/hello"
-                               (slurp-json)
-                               (:message)
-                               (simple-speech-response)))
+(defaction hello_world [_]
+  (-> "https://dbas.cs.uni-duesseldorf.de/api/hello"
+      (slurp-json)
+      (:message)
+      (simple-speech-response)))
 
-(defaction echo_name [request-body] (simple-speech-response "Hello " (get-in request-body [:result :parameters :given-name])))
+(defaction echo_name [request-body]
+  (simple-speech-response "Hello " (get-in request-body [:result :parameters :given-name])))
 
-(defaction how_long [request-body] (simple-speech-response "The word is " (count (get-in request-body [:result :parameters :word])) " characters long."))
+(defaction how_long [request-body]
+  (simple-speech-response "The word is " (count (get-in request-body [:result :parameters :word])) " characters long."))
