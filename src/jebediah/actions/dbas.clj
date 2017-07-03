82d1c95010074653ab728cd5c32c6e1703e662de
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-07-04 00:42:59 +0200

    Update dbas actions to look better in facebook messenger

--- src/jebediah/actions/dbas.clj
+++ src/jebediah/actions/dbas.clj
@@ -9,34 +9,44 @@
 ;; (ai/update-entities! "discussion-topic" (mapv (fn [v] {:value v :synonyms []}) sample-discussions))
 
 (defaction dbas.start-discussions [request]
-  (agent/simple-speech-response "About what position do you want to talk about?"))
+  (agent/simple-speech-response "Ok, lets talk about " (get-in (log/spy :info (ai/get-contexts request)) [:discussion :parameters :discussion-topic.original])
+                                ". Maybe ask for some positions, while I'm developing new skills?"))
 
-(defn fb-list-entry [entry]
+(defn fb-list-entry-button [entry]
   {:title (:title entry)
    :subtitle (:subtitle entry)
    :buttons [{:type "postback"
               :title "This!"
-              :payload (:title entry)}]})
+              :payload (str "Let us talk about " (:title entry))}]})
+
+(defn fb-list-entry [entry]
+  {:title (:title entry)
+   :subtitle (:subtitle entry)})
 
 (defaction dbas.list-discussions [request]
   (case (ai/get-service request)
-    :facebook (fb/simple-list-response (map fb-list-entry
-                                         (take 3 (:issues (dbas/slurp-dbas "query{issues{title, subtitle:info}}")))))
+    :facebook (fb/simple-list-response true (map fb-list-entry-button
+                                              (take 3 (:issues (dbas/slurp-dbas "query{issues{title, subtitle:info}}")))))
     (agent/simple-speech-response "The topics are: " (str/join ", "
                                                             (take 3 (map :title (dbas/get-issues)))) ".")))
 
 
-(defaction dbas.list-discussions.more [_]
-  (let [more-topics (drop 3 (map :title (:issues (dbas/slurp-dbas "query{issues{title, subtitle:info}}"))))
+(defaction dbas.list-discussions.more [request]
+  (let [more-topics (drop 3 (:issues (dbas/slurp-dbas "query{issues{title, subtitle:info}}")))
         topic-count (count more-topics)]
-    (agent/simple-speech-response
-     (if (zero? topic-count)
-       "Sorry, but there are no more topics."
-       (format "Ok, here are %d more topics: %s" topic-count (str/join ", " more-topics))))))
+    (case (ai/get-service request)
+      :facebook (cond
+                  (>= topic-count 2) (fb/simple-list-response false (map fb-list-entry-button more-topics))
+                  (= topic-count 1) (fb/text-response (first more-topics))
+                  :default (fb/text-response "There are no more topics!"))
+      (agent/simple-speech-response
+       (if (zero? topic-count)
+         "Sorry, but there are no more topics."
+         (format "Ok, here are %d more topics: %s" topic-count (str/join ", " (map :title more-topics))))))))
 
 (defaction dbas.info-discussion [request]
   (let [requested-topic (get-in request [:result :parameters :discussion-topic])
-        topic (first (filter #(= (:slug %) requested-topic) (dbas/slurp-dbas "query{issues{title, slug}}")))]
+        topic (first (filter #(= (:slug %) requested-topic) (:issues (dbas/slurp-dbas "query{issues{title, slug, info}}"))))]
     (agent/simple-speech-response
       (if (some? topic)
         (format "Here are more informations about %s: %s" (:title topic) (:info topic))
@@ -44,7 +54,10 @@
 
 (defaction dbas.show-positions-with-discussion [request]
   (let [topic (:discussion (ai/get-contexts request))]
-    (agent/simple-speech-response
-      "Here are some positions for "
-      (get-in topic [:parameters :discussion-topic.original]) ": "
-      (str/join ", " (take 3 (dbas/get-positions-for-issue (get-in topic [:parameters :discussion-topic])))))))
+    (case (ai/get-service request)
+      :facebook
+      (fb/simple-list-response false (map #(fb-list-entry {:title % :subtitle " "}) (take 3 (dbas/get-positions-for-issue (get-in topic [:parameters :discussion-topic])))))
+      (agent/simple-speech-response
+        "Some positions for "
+        (get-in topic [:parameters :discussion-topic.original]) " are: "
+        (str/join ", " (take 3 (dbas/get-positions-for-issue (get-in topic [:parameters :discussion-topic]))))))))
