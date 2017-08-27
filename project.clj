d27e7763bb46e3f5e2c5187f6ff0f2a77d41bacd
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-08-27 20:03:53 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,18 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
                  [ring/ring-json "0.4.0"]
                  [ring-logger "0.7.7"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "3.6.1"]]
+                 [clj-http "3.7.0"]
+                 [clj-fuzzy "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]
-            [lein-kibit "0.1.5"]]
+            [lein-kibit "0.1.5"]
+            [jonase/eastwood "0.2.4"]
+            [lein-ancient "0.6.10"]]
   :source-paths ["src" "lib/apiai-clj/src"]
   :ring {:handler jebediah.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.0"]]}})
+                                  [ring/ring-mock "0.3.1"]]}})
