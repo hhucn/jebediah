c00e39316c91a88486178afa3a503c7c70186187
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-02-24 12:01:18 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [compojure "1.6.0"]
                  [ring/ring-defaults "0.3.1"]
@@ -10,9 +10,9 @@
                  [ring-logger "0.7.7"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.7.0"]
-                 [clj-fuzzy "0.4.0"]]
+                 [clj-fuzzy "0.4.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :source-paths ["src"]
   :ring {:handler jebediah.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.1"]]}})
+                                  [ring/ring-mock "0.3.2"]]}})
