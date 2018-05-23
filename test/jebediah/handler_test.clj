39206fc9c5519cfde4a5b3b1173ef5202cd07f74
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-05-23 16:26:12 +0200

    Adjust tests to jebs demo-page

--- test/jebediah/handler_test.clj
+++ test/jebediah/handler_test.clj
@@ -1,14 +1,16 @@
 (ns jebediah.handler-test
   (:require [clojure.test :refer :all]
+            [clojure.string :refer [starts-with?]]
             [ring.mock.request :as mock]
             [jebediah.handler :refer [app]]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= 200 (:status response)))
-      (is (= "Hello World" (:body response)))))
+(deftest test-GET
+  (testing "GET"
+    (testing "main route"
+      (let [response (app (mock/request :get "/"))]
+        (is (= 200 (:status response)))
+        (is (starts-with? (:body response) "<iframe"))))))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+(deftest test-POST
+  (testing "POST"
+    (testing "spec")))
